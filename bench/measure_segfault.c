a3420cfcd45cb888cb4f8541f5143fd8c1ccfe9a
Syntaf <gmercer015@gmail.com>
2016-12-02 12:46:15 -0800

    refactor measure_segfault to have user entered size

--- bench/measure_segfault.c
+++ bench/measure_segfault.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <time.h>
+#include <string.h>
 
 #include "clat.h"
 #include "clock.h"
@@ -21,7 +22,7 @@ void create_segfault(void *fd_addr, int fd, void *c);
 unsigned long seg_fault_ns;
 unsigned long pread_ns;
 
-int main()
+int main(int argc, char** argv)
 {
     int fd, i;
     size_t mapping_size = 0x4000;
@@ -30,9 +31,33 @@ int main()
     struct stat sb;
     void *fd_addr;
     char c[1];
+    char cline_buff[13];
+    char cline_val[100];
     volatile int j;
 
     int test_size = 100;
+
+    if(argc == 1) {
+        printf("Usage: ./measure_segfault --test-size=<size>\n");
+        return 0;
+    } else if (argc == 2) {
+        strncpy(cline_buff, argv[1], 12);
+        cline_buff[12] = '\0';
+        printf("%s\n", cline_buff);
+        if(strcmp(cline_buff, "--test-size=") == 0) {
+            strncpy(cline_buff, argv[1] + 12, (strlen(argv[1])-12));
+            cline_buff[strlen(argv[1])-12] = '\0';
+            test_size = atoi(cline_buff);
+        } else {
+            printf("Invalid argument \"%s\"\n", cline_buff);
+            return 0;
+        }
+    } else {
+        printf("Too many arguments supplied\n");
+        return 0;
+    }
+
+    printf("Running with test size: %d...\n", test_size);
 
     seg_fault_ns = pread_ns = 0;
 
